c84b99908d208e072a3edf942e9df5181cff4182
Sammy Nyakabau <nyakabausammy8@gmail.com>
2020-09-30 23:54:59 +0530

    Initial Commit 🚀

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+const devConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID"
+  };
+  
+  const prodConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DATABASE_URL",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID"
+  };
+  
+  export {
+    devConfig,
+    prodConfig
+  }
